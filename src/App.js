b7f82c96c06379a10bc1da4336d0d1f75b20b17d
Silvio Coutinho <silviofc79@gmail.com>
2020-05-06 23:00:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount() {
+    let token = localStorage.getItem('token');
+    if (!token) {
+      const login = await axios.post('http://localhost:3001/users/login', {
+        email: 'tuliofaria@devpleno.com',
+        passwd: 'abc123',
+      });
+      token = login.data.token;
+      localStorage.setItem('token', token);
+    }
+    const decoded = jwtDecode(token);
+    console.log(decoded);
+  }
+  render() {
+    return (
+      <div className='App'>
+        <p>OK</p>
+      </div>
+    );
+  }
 }
 
 export default App;
